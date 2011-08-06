9396ac7abf37dd4cde171afa60a1c5cf1e4695f4
miguelpeixe <miguspeixe@gmail.com>
2011-08-06 22:03:28 +0000

    isolated auto-resize js system and improved tab js system

---
+++ js/cardume.dimensions.js
@@ -0,0 +1,228 @@
+$(document).ready(function() {
+	function setDimensions() {
+
+		var fullWidth = $(window).width();
+		var fullHeight = $(window).height();
+
+		$("img.bg").each(
+			function() {
+
+				var originalWidth = $(this).width();
+				var originalHeight = $(this).height();
+
+				var imgHeight = (originalHeight * fullWidth) / originalWidth;
+				var imgWidth = (originalWidth * fullHeight) / originalHeight;
+
+				if(imgHeight < $(window).height()) {
+
+					$(this).width(imgWidth).height(fullHeight);
+
+				} else {
+
+					$(this).width(fullWidth).height(imgHeight);
+
+				}
+
+			}
+		);
+
+		$("h1 img").each(
+			function() {
+
+				var originalWidth = $(this).width();
+				var originalHeight = $(this).height();
+
+				var imgWidth = fullWidth / 6.315;
+				var imgHeight = imgWidth / 0.912;
+
+				$(this).width(imgWidth).height(imgHeight);
+
+				if( $(this).width() <= 150) {
+
+					$(this).width(150).height(164);
+
+				}
+				
+
+			}
+		);
+
+		$("#home p.info").each(
+			function() {
+
+				var fontSize = fullWidth * 0.010;
+				var fontTop = fullWidth * 0.095;
+				var fontLeft = fullWidth * 0.234;
+
+				$(this).css('font-size', fontSize).css('top', fontTop).css('left', fontLeft);
+
+				if(fontSize <= 9) {
+
+					$(this).css('font-size', 9);
+
+					if(fontLeft <= 200) {
+
+						$(this).css('left', 200);
+
+					}
+
+					if(fontTop <= 85) {
+
+						$(this).css('top', 85);
+
+					}
+
+				}
+			}
+		);
+
+		if($.browser.msie) {
+
+			$("#home p.msie").each(
+				function() {
+
+					var fontSize = fullWidth * 0.010;
+					var fontMargin = fullWidth / 109.4;
+
+					$(this).css({
+						fontSize: fontSize,
+						padding: fontMargin
+					});
+
+					if(fontSize <= 9) {
+
+						$(this).css('font-size', 9);
+
+					}
+				}
+			);
+
+		}
+
+		$("#home #sobre").each(
+			function() {
+
+				var fontSize = fullWidth * 0.010;
+				var fontMargin = fullWidth / 27.35;
+				var fontMarginHalf = fullWidth / 54.7;
+				var liPadding = fullWidth / 109.4; 
+
+				$(this).css('font-size', fontSize);
+
+				$(this).find('p').css({
+					margin: fontMarginHalf + 'px ' + fontMargin + 'px'
+				});
+
+				$(this).find('p:first-child').css({
+					marginTop: fontMargin
+				});
+
+				$(this).find('h2').css({
+					margin: '0 ' + fontMargin + 'px'
+				});
+
+				$(this).find('li').css({
+					padding: liPadding
+				});
+
+				$(this).find('ul').css({
+					margin: fontMarginHalf + 'px ' + fontMargin + 'px ' + fontMargin + 'px'
+				});
+
+				if(fontSize <= 9) {
+
+					$(this).css('font-size', 9);
+
+				}
+
+			}
+		);
+
+		$("#home #trabalhos img").each(
+			function() {
+
+				var tabWidth = $("#trabalhos").width();
+
+				var originalWidth = $(this).width();
+				var originalHeight = $(this).height();
+
+				var imgWidth = tabWidth-80;
+				var imgHeight = imgWidth / ( originalWidth / originalHeight );
+
+				$(this).width(imgWidth).height(imgHeight);
+
+			}
+		);
+
+		$(".loading").each(
+			function() {
+
+				var fontSize = fullWidth * 0.010;
+
+				$(this).css({
+					fontSize: fontSize
+				});
+
+			}
+		);
+
+		$("#home #contato").each(
+			function() {
+
+				var fontSize = fullWidth * 0.012;
+				var formMargin = fullWidth / 27.35;
+				var inputMargin = fullWidth / 54.7;
+				var inputPadding = fullWidth / 109.4;
+				var textareaHeight = fullHeight / 2.386;
+
+				var inputWidth = $("#contato").width() - ( inputPadding * 2 ) - ( formMargin * 2 );
+
+				$(this).find('form').css({
+					margin: formMargin
+				});
+
+				$(this).find('input, textarea').css({
+					fontSize: fontSize,
+					padding: inputPadding,
+					marginBottom: inputMargin
+				}).width(inputWidth);
+
+				$(this).find('textarea').css({
+					height: textareaHeight
+				});
+
+				if(fontSize <= 9) {
+
+					$(this).find('input, textarea').css('font-size', 9);
+
+				}
+
+			}
+		);
+
+		$(".tab").each(
+			function() {
+
+				var tabRight = parseInt($(this).css('right'), 10);
+				var tabSize = $(this).width();
+
+				if( tabRight < 0 ) {
+
+					$(this).css({
+						right: -tabSize
+					});
+
+				}
+			}
+		);
+
+	}
+
+	setDimensions();
+
+	$(window).resize(
+		function() {
+			setDimensions();
+		}
+	);
+});
--- js/cardume.js
+++ js/cardume.js
@@ -1,294 +1,79 @@
 $(document).ready(function() {
 
 	if($.browser.msie) {
+
 		$("p.msie").fadeIn('fast');
+
 	}
 
 	var tabSize = $("#home .tab").width();
 
 	$("#home .tab").css({
+
 		right: -tabSize
+
 	});
-
-	function setDimensions() {
-
-		var fullWidth = $(window).width();
-		var fullHeight = $(window).height();
-
-		$("img.bg").each(
-			function() {
-
-				var originalWidth = $(this).width();
-				var originalHeight = $(this).height();
-
-				var imgHeight = (originalHeight * fullWidth) / originalWidth;
-				var imgWidth = (originalWidth * fullHeight) / originalHeight;
-
-				if(imgHeight < $(window).height()) {
-
-					$(this).width(imgWidth).height(fullHeight);
-
-				} else {
-
-					$(this).width(fullWidth).height(imgHeight);
-
-				}
-
-			}
-		);
-
-		$("h1 img").each(
-			function() {
-
-				var originalWidth = $(this).width();
-				var originalHeight = $(this).height();
-
-				var imgWidth = fullWidth / 6.315;
-				var imgHeight = imgWidth / 0.912;
-
-				$(this).width(imgWidth).height(imgHeight);
-
-				if( $(this).width() <= 150) {
-
-					$(this).width(150).height(164);
-
-				}
-				
-
-			}
-		);
-
-		$("#home p.info").each(
-			function() {
-
-				var fontSize = fullWidth * 0.010;
-				var fontTop = fullWidth * 0.095;
-				var fontLeft = fullWidth * 0.234;
-
-				$(this).css('font-size', fontSize).css('top', fontTop).css('left', fontLeft);
-
-				if(fontSize <= 9) {
-
-					$(this).css('font-size', 9);
-
-					if(fontLeft <= 200) {
-
-						$(this).css('left', 200);
-
-					}
-
-					if(fontTop <= 85) {
-
-						$(this).css('top', 85);
-
-					}
-
-				}
-			}
-		);
-
-		if($.browser.msie) {
-
-			$("#home p.msie").each(
-				function() {
-
-					var fontSize = fullWidth * 0.010;
-					var fontMargin = fullWidth / 109.4;
-
-					$(this).css({
-						fontSize: fontSize,
-						padding: fontMargin
-					});
-
-					if(fontSize <= 9) {
-
-						$(this).css('font-size', 9);
-
-					}
-				}
-			);
-
-		}
-
-		$("#home #sobre").each(
-			function() {
-
-				var fontSize = fullWidth * 0.010;
-				var fontMargin = fullWidth / 27.35;
-				var fontMarginHalf = fullWidth / 54.7;
-				var liPadding = fullWidth / 109.4; 
-
-				$(this).css('font-size', fontSize);
-
-				$(this).find('p').css({
-					margin: fontMarginHalf + 'px ' + fontMargin + 'px'
-				});
-
-				$(this).find('p:first-child').css({
-					marginTop: fontMargin
-				});
-
-				$(this).find('h2').css({
-					margin: '0 ' + fontMargin + 'px'
-				});
-
-				$(this).find('li').css({
-					padding: liPadding
-				});
-
-				$(this).find('ul').css({
-					margin: fontMarginHalf + 'px ' + fontMargin + 'px ' + fontMargin + 'px'
-				});
-
-				if(fontSize <= 9) {
-
-					$(this).css('font-size', 9);
-
-				}
-
-			}
-		);
-
-		$("#home #trabalhos img").each(
-			function() {
-
-				var tabWidth = $("#trabalhos").width();
-
-				var originalWidth = $(this).width();
-				var originalHeight = $(this).height();
-
-				var imgWidth = tabWidth-80;
-				var imgHeight = imgWidth / ( originalWidth / originalHeight );
-
-				$(this).width(imgWidth).height(imgHeight);
-
-			}
-		);
-
-		$(".loading").each(
-			function() {
-
-				var fontSize = fullWidth * 0.010;
-
-				$(this).css({
-					fontSize: fontSize
-				});
-
-			}
-		);
-
-		$("#home #contato").each(
-			function() {
-
-				var fontSize = fullWidth * 0.012;
-				var formMargin = fullWidth / 27.35;
-				var inputMargin = fullWidth / 54.7;
-				var inputPadding = fullWidth / 109.4;
-				var textareaHeight = fullHeight / 2.386;
-
-				var inputWidth = $("#contato").width() - ( inputPadding * 2 ) - ( formMargin * 2 );
-
-				$(this).find('form').css({
-					margin: formMargin
-				});
-
-				$(this).find('input, textarea').css({
-					fontSize: fontSize,
-					padding: inputPadding,
-					marginBottom: inputMargin
-				}).width(inputWidth);
-
-				$(this).find('textarea').css({
-					height: textareaHeight
-				});
-
-				if(fontSize <= 9) {
-
-					$(this).find('input, textarea').css('font-size', 9);
-
-				}
-
-			}
-		);
-
-		$(".tab").each(
-			function() {
-
-				var tabRight = parseInt($(this).css('right'), 10);
-				var tabSize = $(this).width();
-
-				if( tabRight < 0 ) {
-
-					$(this).css({
-						right: -tabSize
-					});
-
-				}
-			}
-		);
-
-		hoverScroll("#sobre", "#sobre .content");
-		hoverScroll("#trabalhos", "#trabalhos ul");
-
-	}
-
-	setDimensions();
-
-	$(window).resize(
-		function() {
-			setDimensions();
-		}
-	);
 
 	$("#home > p > a").addClass('inactive');
 
+	function hideTabs(currentTab) {
 
-	function hideTabs() {
+		if( !( $("#home p a.active").length ) ) return false;
 
-		var itemOffset = $("#home p a.active").position();
-		var itemTop = $("#home p a.active").attr('rel');
-		var itemLeft = itemOffset.left;
-		var tabSize = $('.tab').width();
+		$("#home p a.active").each(function() {
 
-		if($.browser.msie) {
+			var tabID = $(this).attr('class').split(' ');
+			var tabID = tabID[0];
 
-			$("#home p a.active").css({
-				width: 'auto',
-				fontSize: '100%',
-				borderWidth: '1px',
-				top: itemTop,
-				left: itemLeft,
-				paddingBottom: 0
-			}).removeClass('active').addClass('inactive');
+		//	var currentLocation = window.location.hash.split('/');
+		//	if( $(this).hasClass(currentLocation[1]) ) return false;
 
-		} else {
+			var itemOffset = $(this).position();
+			var itemTop = $(this).attr('rel');
+			var itemLeft = itemOffset.left;
+			var tabSize = $(this).width();
 
-			$("#home p a.active").animate({
-				width: 150,
-				fontSize: '100%',
-				borderWidth: '1px',
-				top: itemTop,
-				left: itemLeft,
-				paddingBottom: 0
-			}, 'fast', function() {
-				$("#home p a.active").removeClass('active').addClass('inactive');
-			});
+			if($.browser.msie) {
 
-		}
+				$(this).css({
+					width: 'auto',
+					fontSize: '100%',
+					borderWidth: '1px',
+					top: itemTop,
+					left: itemLeft,
+					paddingBottom: 0
+				}).removeClass('active').addClass('inactive');
 
-		$("#home .tab").animate({
-			right: -tabSize
-		}, 'fast');
+			} else {
 
-		$("#home p span").animate({
-			opacity: '1'
-		}, 'fast');
+				$(this).animate({
+					width: 150,
+					fontSize: '100%',
+					borderWidth: '1px',
+					top: itemTop,
+					left: itemLeft,
+					paddingBottom: 0
+				}, 'fast', function() {
+					$(this).removeClass('active').addClass('inactive');
+				});
 
-		$("img.bg").animate({
-			opacity: '1'
-		}, 'fast');
+			}
 
-		$('.bg, h1, p.info').removeClass('close');
+			$("#home #" + tabID + ".tab").animate({
+				right: -tabSize
+			}, 'fast');
+
+			$("#home p span").animate({
+				opacity: '1'
+			}, 'fast');
+
+			$("img.bg").animate({
+				opacity: '1'
+			}, 'fast');
+
+			$('.bg, h1, p.info').removeClass('close');
+
+		});
 
 	}
 
@@ -296,11 +81,7 @@ $(document).ready(function() {
 
 		tabLink = typeof(tabLink) != 'undefined' ? tabLink : '#home p a.' + tabID;
 
-		if($("#home p a.active").length) {
-
-			hideTabs();
-
-		}
+		hideTabs(tabID); // esconde tabs e envia tab selecionada para evitar bug
 
 		// Começa animacao do clicado
 
@@ -357,9 +138,7 @@ $(document).ready(function() {
 
 		$('.close').click(function() {
 
-			goHome();
-
-			return false;
+		//	goHome();
 
 		});
 
@@ -367,9 +146,11 @@ $(document).ready(function() {
 
 	function goHome() {
 
+		if(window.location.hash != '#!/home') window.location = '#!/home';
+
 		hideTabs();
 
-		window.location = '#!/home';
+		return false;
 
 	}
 
@@ -480,7 +261,9 @@ $(document).ready(function() {
 		var location = location.split('/');
 
 		if(location[1] != 'home') {
+
 			doTab(location[1]);
+
 		}
 
 	});
